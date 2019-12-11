1f63759468900ed525771d98eb51bb10d85b2ab2
yashpatel5566789 <yashpatel5566789@gmail.com>
2019-12-11 22:24:51 +0530

    Login front page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ValidatedLoginForm from "./components/Forms/ValidatedLoginForm"
+import styles from "./styles/styles.css"
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Login Form</h1>
+    <ValidatedLoginForm />
+  </div>
+  );
+}
+
+export default App;
