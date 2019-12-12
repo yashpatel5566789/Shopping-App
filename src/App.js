3acb068f3438cddf3d2e80a2ee9d3bc432e97bf3
yashpatel5566789 <yashpatel5566789@gmail.com>
2019-12-13 00:03:06 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ValidatedLoginForm from "./components/Forms/ValidatedLoginForm"
+import ValidatedSignupForm from "./components/Forms/ValidatedSignupForm"
 import styles from "./styles/styles.css"
-
-
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import Header from "./components/Forms/Header"
 import './App.css';
+import { LoginForm } from './components/Forms/LoginForm';
+import index from "./index";
+
 
 function App() {
   return (
+    
+    <Router>
     <div className="App">
+      <Header />
     <h1> Login Form</h1>
-    <ValidatedLoginForm />
+    <Route path="/" exact component={ValidatedLoginForm}/>
+    
+    <Route path="/signup" component={ValidatedSignupForm}/>
+    
+
   </div>
+  </Router>
   );
 }
 
