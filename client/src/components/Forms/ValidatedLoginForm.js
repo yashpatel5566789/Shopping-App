287ca721ce868b08114bfd5c75920059215f2a23
yashpatel5566789 <yashpatel5566789@gmail.com>
2019-12-17 16:34:41 +0530

    pages

--- client/src/components/Forms/ValidatedLoginForm.js
+++ client/src/components/Forms/ValidatedLoginForm.js
@@ -2,39 +2,30 @@ import React from "react";
 import { Formik } from "formik";
 import * as EmailValidator from "email-validator";
 import * as Yup from "yup";
+import { Grid, Transition } from 'semantic-ui-react';
 
-const ValidatedSignupForm = () => (
+const ValidatedLoginForm = () => (
   <Formik
-    initialValues={{ username:"",email: "", password: "", password1:"" }}
+    initialValues={{ email: "", password: "" }}
     onSubmit={(values, { setSubmitting }) => {
       setTimeout(() => {
-        console.log("Signup in", values);
+        console.log("Logging in", values);
         setSubmitting(false);
       }, 500);
     }}
 
+    
 
     validationSchema={Yup.object().shape({
-        username: Yup.string()
-        .required("Required"),
       email: Yup.string()
         .email()
         .required("Required"),
       password: Yup.string()
         .required("No password provided.")
         .min(8, "Password is too short - should be 8 chars minimum.")
-        .matches(/(?=.*[0-9])/, "Password must contain a number."),
-        password1: Yup.string()
-        .required("No password provided.")
-        .min(8, "Password is too short - should be 8 chars minimum.")
         .matches(/(?=.*[0-9])/, "Password must contain a number.")
-        
-      
-      
-        
     })}
   >
-
     {props => {
       const {
         values,
@@ -47,20 +38,7 @@ const ValidatedSignupForm = () => (
       } = props;
       return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Username</label>
-          <input
-            name="username"
-            type="text"
-            placeholder="Enter your username"
-            value={values.username}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.email && touched.email && "error"}
-          />
-          
-          {errors.username && touched.username && (
-            <div className="input-feedback">{errors.username}</div>
-          )}
+          <h1>Login Page</h1>
           <label htmlFor="email">Email</label>
           <input
             name="email"
@@ -87,29 +65,18 @@ const ValidatedSignupForm = () => (
           {errors.password && touched.password && (
             <div className="input-feedback">{errors.password}</div>
           )}
-          <label htmlFor="email">Re-enter Password</label>
-          <input
-            name="password1"
-            type="password"
-            placeholder="Enter your Re-enter password"
-            value={values.password1}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.password1 && touched.password1 && "error"}
-          />
-          {errors.password1 && touched.password1 && (
-            <div className="input-feedback">{errors.password1}</div>
-          )}
           <button className="login "type="submit" disabled={isSubmitting}>
             Login
           </button>
-          <button id="signup" type="submit" disabled={isSubmitting}>
+          <button id="signup" type="submit" >
+            
             Signup
           </button>
+          <link to="/SignupForm"></link>
         </form>
       );
     }}
   </Formik>
 );
 
-export default ValidatedSignupForm;
+export default ValidatedLoginForm;
